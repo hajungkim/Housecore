ed717eba8ffca6ced803d97c7d168fe7588f5d4e
이재영 <giver12@naver.com>
2021-05-12 22:14:58 +0900

    merge

--- src/main/java/com/ssafy/happyhouse/controller/AptDealController.java
+++ src/main/java/com/ssafy/happyhouse/controller/AptDealController.java
@@ -1,12 +1,6 @@
 package com.ssafy.happyhouse.controller;
 
-import java.io.IOException;
 import java.sql.SQLException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,6 +20,27 @@ public class AptDealController {
 	@Autowired
 	private AptDealService aptDealService;
 	
+	// {dongcode}
+	// @PathVariable String dongcode,
+	@GetMapping("")
+	public String list(@RequestParam(defaultValue = "1") int pageNo, Model model ) {
+		PageDto pageDto = new PageDto();
+		pageDto.setPageNo(pageNo);
+		
+		SearchDto searchDto = new SearchDto();
+		//searchDto.setDongcode(dongcode);
+		searchDto.setPageNo(pageNo);
+		
+		try {
+			model.addAttribute("result", aptDealService.selectAptDeal(pageDto));
+			//model.addAttribute("dongcode", dongcode);
+			model.addAttribute("dongcode", "");
+			return "boardResult";
+		} catch (SQLException e) {
+			return "index";
+		}
+	}
+	
 	@GetMapping("{dongcode}")
 	public String list(@PathVariable String dongcode, @RequestParam(defaultValue = "1") int pageNo, Model model ) {
 		PageDto pageDto = new PageDto();
@@ -38,49 +53,10 @@ public class AptDealController {
 		try {
 			model.addAttribute("result", aptDealService.selectAptDeal(pageDto));
 			model.addAttribute("dongcode", dongcode);
+			model.addAttribute("dongcode", "");
 			return "boardResult";
 		} catch (SQLException e) {
 			return "index";
 		}
-	}
-	
-	// 이 아래에서 부터는 새로 짜는게 나을듯
-	
-	private void searchedList(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String dongcode = req.getParameter("dongcode");
-		PageDto pageDto = new PageDto();
-		int pageNo = 1;
-		try {
-			pageDto.setPageNo(Integer.parseInt(req.getParameter("pageNo")));
-		}catch (Exception e) {} 
-		SearchDto searchDto = new SearchDto();
-		searchDto.setDongcode(dongcode);
-		searchDto.setPageNo(pageNo);
-		
-		try {
-			req.setAttribute("result", aptDealService.selectAptDealBySearch(searchDto, pageDto));
-			req.setAttribute("dongcode", dongcode);
-
-			req.getRequestDispatcher("/view/boardResult.jsp").forward(req, resp);;
-		} catch (SQLException e) {
-			// 에러 페이지 연결
-			e.printStackTrace();
-		}
-	}
-	private void showList(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-		PageDto pageDto = new PageDto();
-		try {
-			pageDto.setPageNo(Integer.parseInt(req.getParameter("pageNo")));
-		}catch (Exception e) {} 
-		try {
-			req.setAttribute("result", aptDealService.selectAptDeal(pageDto));
-			req.setAttribute("dongcode", "");
-			req.getRequestDispatcher("/view/boardResult.jsp").forward(req, resp);
-		} catch (SQLException e) {
-			// 에러 페이지 연결
-			e.printStackTrace();
-		}
-		
 	}
 }
