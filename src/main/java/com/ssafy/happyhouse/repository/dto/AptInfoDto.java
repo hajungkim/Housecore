4b2f7eff1a3c11c457ea9e98856d2c8170e94bf6
이재영 <giver12@naver.com>
2021-05-22 16:32:34 +0900

    Update: 아파트 관련 Dto 수정

--- src/main/java/com/ssafy/happyhouse/repository/dto/AptInfoDto.java
+++ src/main/java/com/ssafy/happyhouse/repository/dto/AptInfoDto.java
@@ -2,11 +2,7 @@ package com.ssafy.happyhouse.repository.dto;
 
 public class AptInfoDto {
 	private int no;
-	private String dong;
 	private String AptName;
-	private String code;
-	private String buildYear;
-	private String jibun;
 	private String lat;
 	private String lng;
 	public int getNo() {
@@ -15,35 +11,11 @@ public class AptInfoDto {
 	public void setNo(int no) {
 		this.no = no;
 	}
-	public String getDong() {
-		return dong;
-	}
-	public void setDong(String dong) {
-		this.dong = dong;
-	}
 	public String getAptName() {
 		return AptName;
 	}
 	public void setAptName(String aptName) {
 		AptName = aptName;
-	}
-	public String getCode() {
-		return code;
-	}
-	public void setCode(String code) {
-		this.code = code;
-	}
-	public String getBuildYear() {
-		return buildYear;
-	}
-	public void setBuildYear(String buildYear) {
-		this.buildYear = buildYear;
-	}
-	public String getJibun() {
-		return jibun;
-	}
-	public void setJibun(String jibun) {
-		this.jibun = jibun;
 	}
 	public String getLat() {
 		return lat;
